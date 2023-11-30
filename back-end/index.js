de1bc1f14155db0b8a56816cf769df7a137f92fc
IsmailCharfi <charfi.ismail@gmail.com>
2023-11-30 21:48:57 +0100

    update tests

--- back-end/index.js
+++ back-end/index.js
@@ -3,67 +3,73 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const { Todo } = require("./model");
 
-const app = express();
+const createApp = (port = null) => {
+  const app = express();
 
-app.use(express.json());
-app.use(cors());
+  app.use(express.json());
+  app.use(cors());
 
-app.get("/todos", async (req, res) => {
-  try {
-    const todos = (await Todo.find()).map((x) => x._doc);
-    res.json(todos.map((t) => ({ ...t, id: t._id })));
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+  app.get("/todos", async (req, res) => {
+    try {
+      const todos = (await Todo.find()).map((x) => x._doc);
+      res.json(todos.map((t) => ({ ...t, id: t._id })));
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
 
-app.post("/todos", async (req, res) => {
-  const { title } = req.body;
+  app.post("/todos", async (req, res) => {
+    const { title } = req.body;
 
-  try {
-    await Todo.create({ title });
-    res.json({ message: "Todo added successfully" });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+    try {
+      await Todo.create({ title });
+      res.json({ message: "Todo added successfully" });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
 
-app.patch("/todos/:id", async (req, res) => {
-  const { id } = req.params;
-  const { completed, title } = req.body;
+  app.patch("/todos/:id", async (req, res) => {
+    const { id } = req.params;
+    const { completed, title } = req.body;
 
-  try {
-    const updatedTodo = await Todo.findByIdAndUpdate(
-      id,
-      { completed, title },
-      { new: true }
-    );
-    res.json({ message: "Todo updated successfully" });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+    try {
+      const updatedTodo = await Todo.findByIdAndUpdate(
+        id,
+        { completed, title },
+        { new: true }
+      );
+      res.json({ message: "Todo updated successfully" });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
 
-app.delete("/todos/:id", async (req, res) => {
-  const { id } = req.params;
+  app.delete("/todos/:id", async (req, res) => {
+    const { id } = req.params;
 
-  try {
-    await Todo.findByIdAndDelete(id);
-    res.json({ message: "Todo deleted successfully" });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+    try {
+      await Todo.findByIdAndDelete(id);
+      res.json({ message: "Todo deleted successfully" });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
 
-const PORT = process.env.PORT || 5050;
-const DB_URL = process.env.DB_URL || "mongodb://127.0.0.1:27017/todo";
+  const PORT = port ?? process.env.PORT ?? 5050;
+  const DB_URL = process.env.DB_URL ?? "mongodb://127.0.0.1:27017/todo";
 
-mongoose
-  .connect(DB_URL)
-  .then(() => app.listen(PORT))
-  .then(() => console.log(`Listening on port ${PORT}`))
-  .catch((error) => console.error(error));
+  return mongoose
+    .connect(DB_URL)
+    .then(() => app.listen(PORT))
+    .then((server) => {
+      console.log("Server listening on port " + PORT);
+      return { server, app };
+    })
+    .catch((error) => console.error(error));
+};
+
 
 module.exports = {
-  app,
+  createApp,
 };
