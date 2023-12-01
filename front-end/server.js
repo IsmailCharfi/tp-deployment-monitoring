2ee7a2bc6e241e331a437ab24ff89bab18591320
IsmailCharfi <charfi.ismail@gmail.com>
2023-12-01 01:46:14 +0100

    tf vm

--- front-end/server.js
+++ front-end/server.js
@@ -5,7 +5,7 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, "dist")));
 
 app.get("/api", (req, res) => {
-  res.json({ api: process.env.API_PATH || "http://localhost:5050" });
+  res.json({ api: process.env.API_PATH || "https://api.todo.charfi.me/" });
 });
 
 const PORT = process.env.PORT || 8080;
--- main.tf
+++ main.tf
@@ -2,65 +2,69 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "devops-rg" {
-  name     = "tp-devops"
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
   location = "East US"
 }
 
-resource "azurerm_virtual_network" "devops-vn" {
-  name                = "devops-network"
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.devops-rg.location
-  resource_group_name = azurerm_resource_group.devops-rg.name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 }
 
-resource "azurerm_subnet" "devops-sub" {
-  name                 = "devops-subnet"
-  resource_group_name  = azurerm_resource_group.devops-rg.name
-  virtual_network_name = azurerm_virtual_network.devops-vn.name
-  address_prefixes    = ["10.0.0.0/24"]
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes    = ["10.0.2.0/24"]
 }
 
-resource "azurerm_public_ip" "devops-pip" {
-  name                = "devops-public-ip"
-  resource_group_name = azurerm_resource_group.devops-rg.name
-  location            = azurerm_resource_group.devops-rg.location
+resource "azurerm_public_ip" "example" {
+  name                = "example-public-ip"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
   allocation_method   = "Dynamic"
 }
 
-resource "azurerm_network_interface" "devops-nic" {
-  name                = "devops-nic"
-  location            = azurerm_resource_group.devops-rg.location
-  resource_group_name = azurerm_resource_group.devops-rg.name
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.devops-sub.id
+    subnet_id                     = azurerm_subnet.example.id
     private_ip_address            = "10.0.0.5"
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.devops-pip.id
+    public_ip_address_id          = azurerm_public_ip.example.id
   }
 }
 
-resource "azurerm_windows_virtual_machine" "devops-wvm" {
-  name                  = "devops-vm"
-  resource_group_name   = azurerm_resource_group.devops-rg.name
-  location              = azurerm_resource_group.devops-rg.location
-  size                  = "Standard_D2s_v3"
-  admin_username        = "ismail"
-  admin_password        = "#Ismail123456"
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "example-vm"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  size                = "Standard_DS1_v2"
+  disable_password_authentication = "false"
+  admin_username      = "ismail"
+  admin_password      = "#Ismail123456"
 
-  network_interface_ids = [azurerm_network_interface.devops-nic.id]
+  network_interface_ids = [azurerm_network_interface.example.id]
 
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
 
+
   source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2019-Datacenter"
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
     version   = "latest"
   }
+
+  computer_name  = "hostname"
 }
